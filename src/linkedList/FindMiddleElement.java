5a9253fefb1b6e6a0ff0f28c62fe37c367a506c9
Praks1987 <you@example.com>
2019-05-20 23:55:44 +0530

    find middle

---
+++ src/linkedList/FindMiddleElement.java
@@ -0,0 +1,65 @@
+package linkedList;
+
+public class FindMiddleElement {
+
+	Node head;
+
+	class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+
+	}
+
+	public void create(int data) {
+		Node newnode = new Node(data);
+		if (head == null) {
+			head = newnode;
+		} else {
+			Node current = head;
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = newnode;
+		}
+
+	}
+
+	public void print() {
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.data + " , ");
+			current = current.next;
+		}
+		System.out.println(" ");
+	}
+
+	public void findMiddle() {
+		Node front = head;
+		Node mid = head;
+		while (front.next != null) {
+			mid = mid.next;
+			front = front.next.next;
+		}
+		System.out.println(" middle number is " + mid.data);
+	}
+
+	public static void main(String[] args) {
+		FindMiddleElement fe = new FindMiddleElement();
+		fe.create(10);
+		fe.create(20);
+		fe.create(15);
+		fe.create(30);
+		fe.create(67);
+		fe.create(14);
+		fe.create(37);
+		fe.print();
+		fe.findMiddle();
+
+	}
+
+}
